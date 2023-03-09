4497b9f9558676a2cc29a19946397f295a6e8540
henok26 <henokaniguse1931@gmail.com>
2023-03-09 18:16:42 +0300

    strings

--- Variables.py
+++ Variables.py
@@ -3,3 +3,10 @@ PlateNo = 1234
 Distance = 299.9
 IsAvailableAfterNoon = False
 print(Route1)
+print(len(Route1))
+print(Route1[-2])
+print(Route1[-5])
+print(Route1[0:11])
+print(Route1[:])
+print(Route1[:11])
+print(Route1[0:])
