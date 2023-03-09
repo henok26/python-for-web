9a797d24392ac81f4f3379415c6748e003bb05a6
henok26 <henokaniguse1931@gmail.com>
2023-03-10 00:13:41 +0300

    If statements

---
+++ Control_Flow.py
@@ -0,0 +1,14 @@
+Route1 = "Debremarkos To Addis Ababa"
+Route2 = "Addis Ababa To Bahir Dar"
+Route3 = "Addis Ababa To Gondar"
+Bus_Id1 = "Fm1"
+Bus_Id2 = "Fm2"
+Bus_Id3 = "Fm3"
+Bus_Id = input("enter bus id:")
+
+if int(Bus_Id) == 1:
+    print(Bus_Id1)
+elif int(Bus_Id) == 2:
+    print(Bus_Id2)
+else:
+    print(Bus_Id3)
