45a6b7780721043873c0a8d4823a79617b7ddcb7
henok26 <henokaniguse1931@gmail.com>
2023-03-09 20:40:09 +0300

    Escape character

--- Variables.py
+++ Variables.py
@@ -1,6 +1,7 @@
 Route1 = "Debremarkos to Addis Ababa"
 PlateNo = 1234
 Distance = 299.9
+Escape = "welcome to \"fm\""
 IsAvailableAfterNoon = False
 print(Route1)
 print(len(Route1))
@@ -10,3 +11,4 @@ print(Route1[0:11])
 print(Route1[:])
 print(Route1[:11])
 print(Route1[0:])
+print(Escape)
